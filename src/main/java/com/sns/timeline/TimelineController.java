81d9b2909d435d15058f7a1a5c6947a519fb6500
DESKTOP-RE0A8DL\dhyoon <ehdgml3913@naver.com>
2024-07-16 22:37:00 +0900

    sns 글 목록 2시간?

--- src/main/java/com/sns/timeline/TimelineController.java
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -1,13 +1,33 @@
 package com.sns.timeline;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class TimelineController {
 
+	@Autowired
+	private PostBO postBO;
+	
 	@GetMapping("/timeline/timeline-view")
-	public String timelineView() {
+	public String timelineView(HttpSession session, Model model) {
+//		if (session.getAttribute("userId") == null) {
+//			return "redirect:/user/sign-in-view";
+//		}
+		
+		List<PostEntity> postList = postBO.getPostEntityList();
+		
+		model.addAttribute("postList", postList);
+		
 		return "timeline/timeline";
 	}
 }
